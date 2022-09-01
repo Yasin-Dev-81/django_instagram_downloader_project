9e6d7dcae1b5131148e1ef39a3725e0120aadf61
Yasin Dev 81 <76453371+Yasin-Dev-81@users.noreply.github.com>
2022-09-01 16:21:13 +0430

    Update views.py

--- insta_telegram/views.py
+++ insta_telegram/views.py
@@ -26,7 +26,7 @@ def webhook_view(request):
         if request.user.is_staff:
             webhook_url = request.build_absolute_uri()
             tbot.remove_webhook()
-            tbot.set_webhook(webhook_url)
+            tbot.set_webhook(url=webhook_url, timeout=15)
             return HttpResponse(f"set webhook: {webhook_url}")
         else:
             print('--- else-else!')
