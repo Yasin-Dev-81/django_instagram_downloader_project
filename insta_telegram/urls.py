2eeb075ce823f3116f21b6dc8212503a42f1dcf9
Yasin Dev 81 <76453371+Yasin-Dev-81@users.noreply.github.com>
2022-09-01 16:19:22 +0430

    Update urls.py

--- insta_telegram/urls.py
+++ insta_telegram/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('webhook/', views.webhook_view, name="telegram_bot_url"),
+    path('webhook/remove/', views.remove_webhook, name="remove_telebot_webhook_url"),
 ]
