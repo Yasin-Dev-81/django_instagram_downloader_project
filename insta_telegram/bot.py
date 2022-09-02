2ec4510e65b171647b65be22fc7d774c3dddc5ad
Yasin Dev 81 <76453371+Yasin-Dev-81@users.noreply.github.com>
2022-09-02 12:59:56 +0430

    Update bot.py

--- insta_telegram/bot.py
+++ insta_telegram/bot.py
@@ -11,13 +11,14 @@ import datetime
 
 from insta_web.models import InstagramData
 
+
 bot = telebot.TeleBot(TELEGRAM_TOKEN, threaded=False)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     print(message)
-    bot.reply_to(message, "Howdy, how are you doing? \U000023F3")
+    bot.reply_to(message, "Howdy, how are you doing?")
 
 
 @bot.message_handler(func=lambda message: True)
@@ -26,40 +27,58 @@ def instagram(message):
     print('--- started with url:', message.text)
     insta_cl = DirectoryDownload(
         sessionid_list=[obj.sessionid for obj in InstagramData.objects.filter(active=True)],
-        folder_path=os.path.join(settings.MEDIA_ROOT, 'instagram_downloaded')
+        folder_path=os.path.join(MEDIA_ROOT, 'instagram_downloaded')
     )
     try:
         insta_cl.url = requests.get(message.text).url
+        print('--- instagram url:', insta_cl.url)
         continue_process = True
     except requests.exceptions.MissingSchema:
         bot.reply_to(message, "Please send Instagram link!")
         continue_process = False
     if ("instagram.com" in insta_cl.url) and continue_process:
         insta_cl.type_inspector_with_url(insta_cl.url, to_get=False)
+        print('--- typed inspected:')
         if insta_cl.downloadable():
             download_message = bot.reply_to(message, 'The download started from the Instagram server...')
             insta_cl.data_inspector_with_url()
-            print(insta_cl.data)
+            print('--- instagram data got from server:', insta_cl.data)
             caption_tel = """
             <i>►username:</i> <a href="https://instagram.com/{username}/">{username}</a>\n<i>📆ctime:</i> <b>{ctime}</b> 
             """.format(
                 username=insta_cl.data.get('user').get('username'),
                 ctime=insta_cl.data.get('taken_at').ctime(),
             )
-            medias_sent = bot.send_media_group(
-                chat_id=message.chat.id,
-                media=insta_cl.input_medias(caption=caption_tel),
-                disable_notification=False,
-                protect_content=False,
-                reply_to_message_id=message.message_id,
-                allow_sending_without_reply=True,
-            )
+            try:
+                bot.send_media_group(
+                    chat_id=message.chat.id,
+                    media=insta_cl.input_medias(caption=caption_tel),
+                    disable_notification=False,
+                    protect_content=False,
+                    reply_to_message_id=message.message_id,
+                    allow_sending_without_reply=True,
+                )
+            except Exception:
+                insta_cl.start()
+                bot.send_message(
+                    chat_id=message.chat.id,
+                    text="This post cannot be uploaded in Telegram, please download from the site!",
+                    disable_notification=False,
+                    protect_content=False,
+                    reply_to_message_id=message.message_id,
+                    allow_sending_without_reply=True,
+                )
             bot.delete_message(chat_id=message.chat.id, message_id=hourglass_message.message_id)
             bot.delete_message(chat_id=message.chat.id, message_id=download_message.message_id)
             # print(medias_sent)
             url_markup = types.InlineKeyboardMarkup(row_width=1)
             url_markup.add(
                 types.InlineKeyboardButton(text="open in instagram", url=message.text)
+            )
+            bot.send_message(
+                chat_id=message.chat.id,
+                text=caption_tel,
+                parse_mode='html',
             )
             bot.send_message(
                 chat_id=message.chat.id,
@@ -76,6 +95,7 @@ def instagram(message):
             bot.reply_to(message, "This link cannot be downloaded with this robot!")
     else:
         bot.reply_to(message, "This link does not belong to Instagram!")
+
 
 
 if __name__ == '__main__':
