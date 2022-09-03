c6958ca8361137ba5e759aac56bfb7d95c4bcfba
Yasin Dev 81 <76453371+Yasin-Dev-81@users.noreply.github.com>
2022-09-03 10:37:08 +0430

    Update models.py

--- insta_web/models.py
+++ insta_web/models.py
@@ -2,11 +2,6 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
-class ActivesManager(models.Manager):
-    def get_queryset(self):
-        return super(ActivesManager, self).get_queryset().filter(active=True)
-
-
 class InstagramData(models.Model):  # data for login and search in instagram
     username = models.CharField(max_length=20)
     sessionid = models.CharField(max_length=80, blank=False, null=False)
@@ -15,10 +10,6 @@ class InstagramData(models.Model):  # data for login and search in instagram
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
-
-    # manager
-    object = models.Manager()
-    actives_manager = ActivesManager()
 
     def __str__(self):
         return self.username
