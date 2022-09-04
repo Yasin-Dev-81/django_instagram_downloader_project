c605dc9b3c6d3fb0d801208c6bef3a31691f1a09
Yasin Dev 81 <76453371+Yasin-Dev-81@users.noreply.github.com>
2022-09-04 09:43:24 +0430

    Update tasks.py

--- insta_telegram/tasks.py
+++ insta_telegram/tasks.py
@@ -27,7 +27,10 @@ class TelTasksForInstaLink:
         self.url = None
 
     def send_hourglass_message(self):
-        self.hourglass_message = self.bot.reply_to(self.message, '\U000023F3')
+        try:
+            self.hourglass_message = self.bot.reply_to(self.message, '\U000023F3')
+        except telebot.apihelper.ApiTelegramException:
+            print('--- user blocked this bot!')
 
     def valid_url(self):
         try:
@@ -40,7 +43,10 @@ class TelTasksForInstaLink:
                 self.bot.reply_to(self.message, "This link does not belong to Instagram!")
                 raise ValueError('Not a Instagram url!')
         except requests.exceptions.MissingSchema:
-            self.bot.reply_to(self.message, "Please send Instagram url!")
+            try:
+                self.bot.reply_to(self.message, "Please send Instagram url!")
+            except telebot.apihelper.ApiTelegramException:
+                print('--- user blocked this bot!')
             raise ValueError('Not a url!')
 
     def inspect_type_and_data(self):
@@ -51,6 +57,8 @@ class TelTasksForInstaLink:
                 self.insta_cl.data_inspector_with_url()
             except instagrapi.exceptions.UnknownError:
                 self.bot.reply_to(self.message, 'Media not found or unavailable or accounts private!')
+            except telebot.apihelper.ApiTelegramException:
+                print('--- user blocked this bot!')
         else:
             self.message.reply_to(self.message, "This link cannot be downloaded with this robot!")
             raise ValueError('This link cannot be downloaded with this robot!')
@@ -67,15 +75,18 @@ class TelTasksForInstaLink:
                 allow_sending_without_reply=True,
             )
         except telebot.apihelper.ApiTelegramException:
-            for tel_input_media in tel_input_medias:
-                self.bot.send_media_group(
-                    chat_id=self.message.chat.id,
-                    media=[tel_input_media],
-                    disable_notification=False,
-                    protect_content=False,
-                    reply_to_message_id=self.message.message_id,
-                    allow_sending_without_reply=True,
-                )
+            try:
+                for tel_input_media in tel_input_medias:
+                    self.bot.send_media_group(
+                        chat_id=self.message.chat.id,
+                        media=[tel_input_media],
+                        disable_notification=False,
+                        protect_content=False,
+                        reply_to_message_id=self.message.message_id,
+                        allow_sending_without_reply=True,
+                    )
+            except telebot.apihelper.ApiTelegramException:
+                print('--- user blocked this bot!')
 
     def send_captions(self):
         caption_tel = """
@@ -84,35 +95,44 @@ class TelTasksForInstaLink:
             username=self.insta_cl.data.get('user').get('username'),
             ctime=self.insta_cl.data.get('taken_at').ctime(),
         )
-        self.bot.send_message(
-            chat_id=self.message.chat.id,
-            text=caption_tel,
-            parse_mode='html',
-        )
-        url_markup = types.InlineKeyboardMarkup(row_width=1)
-        url_markup.add(
-            types.InlineKeyboardButton(text="open in instagram", url=self.url)
-        )
-        self.bot.send_message(
-            chat_id=self.message.chat.id,
-            text="<i>📃caption:</i> <code>{caption}</code>".format(caption=self.insta_cl.data.get('caption_text')),
-            parse_mode='html',
-            reply_markup=url_markup
-        )
+        try:
+            self.bot.send_message(
+                chat_id=self.message.chat.id,
+                text=caption_tel,
+                parse_mode='html',
+            )
+            url_markup = types.InlineKeyboardMarkup(row_width=1)
+            url_markup.add(
+                types.InlineKeyboardButton(text="open in instagram", url=self.url)
+            )
+            self.bot.send_message(
+                chat_id=self.message.chat.id,
+                text="<i>📃caption:</i> <code>{caption}</code>".format(caption=self.insta_cl.data.get('caption_text')),
+                parse_mode='html',
+                reply_markup=url_markup
+            )
+        except telebot.apihelper.ApiTelegramException:
+            print('--- user blocked this bot!')
 
     def send_powered(self):
-        self.bot.send_message(
-            chat_id=self.message.chat.id,
-            text="<i>Powered by</i> <b>{admin}</b>".format(admin='@Yasin_Dev81'),
-            parse_mode='html',
-        )
+        try:
+            self.bot.send_message(
+                chat_id=self.message.chat.id,
+                text="<i>Powered by</i> <b>{admin}</b>".format(admin='@Yasin_Dev81'),
+                parse_mode='html',
+            )
+        except telebot.apihelper.ApiTelegramException:
+            print('--- user blocked this bot!')
 
     def delete_additions_messages(self):
-        self.bot.delete_message(
-            chat_id=self.message.chat.id,
-            message_id=self.hourglass_message.message_id
-        )
-        self.bot.delete_message(
-            chat_id=self.message.chat.id,
-            message_id=self.download_message.message_id
-        )
+        try:
+            self.bot.delete_message(
+                chat_id=self.message.chat.id,
+                message_id=self.hourglass_message.message_id
+            )
+            self.bot.delete_message(
+                chat_id=self.message.chat.id,
+                message_id=self.download_message.message_id
+            )
+        except telebot.apihelper.ApiTelegramException:
+            print('--- user blocked this bot!')
